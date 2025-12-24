fa13e7a62af94876e3641fa477eee74b5e6265bd
Tharindu <tharindu@advicelab.com.au>
2025-12-24 11:03:03 +0530

    Top banner Button updated

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
 import { Layout } from "@/components/layout/Layout";
+import { TopBanner } from "@/components/home/TopBanner";
 import { HeroSlider } from "@/components/home/HeroSlider";
 import { ClientLogos } from "@/components/home/ClientLogos";
 import { ServicesOverview } from "@/components/home/ServicesOverview";
-import { CustomizeSection } from "@/components/home/CustomizeSection";
 import { WhyChooseUs } from "@/components/home/WhyChooseUs";
 import { Testimonials } from "@/components/home/Testimonials";
 import styles from "./Index.module.css";
@@ -10,10 +10,10 @@ import styles from "./Index.module.css";
 const Index = () => {
   return (
     <Layout>
+      <TopBanner />
       <HeroSlider />
       <ClientLogos />
       <ServicesOverview />
-      <CustomizeSection />
       <WhyChooseUs />
       <Testimonials />
     </Layout>
