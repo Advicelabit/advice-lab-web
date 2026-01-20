7fc3c9a379652a99358d1c5897c0cfc9f86a7e77
Shahanshaeek <shahanshaeek@advicelab.com.au>
2026-01-20 11:32:26 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      {/* check */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
